827747c1da7e1e736fba8228c37ca45e5b3dc6a2
rookiecol <juan.diegam@hotmail.com>
2023-03-01 11:34:26 -0500

    Project init

---
+++ contracts/genesis-nft.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.1;
+import "hardhat/console.sol";
+
+contract GenesisNftminter {
+    constructor() {
+        console.log("Here we go freedom!");
+    }
+}
